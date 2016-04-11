952c18e7f3fd519b5e6749549939f8b762edc0c5
Jonathan Fuqua <jfuqua390@gmail.com>
2016-04-11 14:19:58 -0500

    Starting assignment

--- app/heroes/service.js
+++ app/heroes/service.js
@@ -14,7 +14,7 @@ function replace(arr, item) {
 }
 
 export default Ember.Service.extend({
-  apiUrl: `https://tiny-tn.herokuapp.com/collections/heroes`,
+  apiUrl: `https://tiny-tn.herokuapp.com/collections/heroes/ja-heroes`,
   store: [],
   loaded: false,
 
